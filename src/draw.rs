97ad64083fcf879efceded553487fb25067ac65c
Pazzaz <pazzaz.sundqvist@gmail.com>
2026-08-15 20:04:36 +0200

    Improve SVG, add gradient colors

--- src/draw.rs
+++ src/draw.rs
@@ -9,6 +9,103 @@ use svg::{
 // Adjacent SVG elements may display a gap, so we add `EPSILON` overlap in some places
 const EPSILON: f64 = 0.01;
 
+const COLOR_WIDTH: f64 = 0.2;
+
+const IMAGE_WIDTH_Y: f64 = 6.9;
+const IMAGE_HEIGHT_X: f64 = 1.5;
+
+#[derive(Debug, Clone, Copy)]
+struct ImageParams<'a> {
+    x: f64,
+    y: f64,
+    height: f64,
+    width: f64,
+    x_padding: f64,
+    y_padding: f64,
+    id: &'a str,
+    image: &'a str,
+}
+
+fn add_image(
+    document: &mut element::SVG,
+    definitions: &mut element::Definitions,
+    params: ImageParams,
+) {
+    let rect = Rectangle::new()
+        .set("x", params.x)
+        .set("y", params.y)
+        .set("width", params.width)
+        .set("height", params.height)
+        .set("fill", "white");
+    let clip_path = element::ClipPath::new().set("id", params.id).add(rect);
+
+    let img = element::Image::new()
+        .set("x", params.x - params.x_padding)
+        .set("y", params.y - params.y_padding)
+        .set("width", params.width + params.x_padding * 2.0)
+        .set("href", format!("data:image/jpeg;base64,{}", params.image))
+        .set("clip-path", format!("url(#{})", params.id));
+
+    document.append(img);
+    definitions.append(clip_path);
+}
+
+#[derive(Debug, Clone, Copy)]
+struct BoxParams {
+    x: f64,
+    y: f64,
+    width: f64,
+    height: f64,
+}
+
+fn add_color_box(document: &mut element::SVG, params: BoxParams, color: &str) {
+    let rect = Rectangle::new()
+        .set("x", params.x)
+        .set("y", params.y)
+        .set("width", params.width)
+        .set("height", params.height)
+        .set("fill", color);
+    document.append(rect);
+}
+
+fn add_gradient_box(
+    document: &mut element::SVG,
+    definitions: &mut element::Definitions,
+    params: BoxParams,
+    id: &str,
+    colors: &[Color],
+    rotate_gradient: bool,
+) {
+    assert!(colors.len() > 1);
+    let mut gradient = element::LinearGradient::new().set("id", id);
+
+    if rotate_gradient {
+        gradient.assign("x1", 0);
+        gradient.assign("x2", 0);
+        gradient.assign("y1", 1);
+        gradient.assign("y2", 0);
+    }
+
+    let len = (colors.len() - 1) as f64;
+    for (i, color) in colors.iter().enumerate() {
+        gradient.append(
+            element::Stop::new()
+                .set("offset", format!("{}%", 100.0 * i as f64 / len))
+                .set("stop-color", color.to_string()),
+        );
+    }
+
+    let rect = Rectangle::new()
+        .set("x", params.x)
+        .set("y", params.y)
+        .set("width", params.width)
+        .set("height", params.height)
+        .set("fill", format!("url(#{})", id));
+
+    definitions.append(gradient);
+    document.append(rect);
+}
+
 pub fn create_svg(data: &Data, x_images: &[String], y_images: &[String]) -> impl svg::Node {
     let box_width: f64 = 1.4;
     let scale = 30.0;
@@ -25,11 +122,8 @@ pub fn create_svg(data: &Data, x_images: &[String], y_images: &[String]) -> impl
         }
     }
 
-    let margin_top = 2.0;
-    let margin_left = 8.0;
-
-    let width = x_len as f64 * box_width + margin_left;
-    let height = y_len as f64 + margin_top;
+    let width = x_len as f64 * box_width + IMAGE_WIDTH_Y + COLOR_WIDTH;
+    let height = y_len as f64 + IMAGE_HEIGHT_X + COLOR_WIDTH;
 
     let mut document = Document::new()
         .set("width", scale * width)
@@ -42,71 +136,83 @@ pub fn create_svg(data: &Data, x_images: &[String], y_images: &[String]) -> impl
     for (i, image) in x_images.iter().enumerate() {
         let id = format!("x-{i}");
 
-        let rect = Rectangle::new()
-            .set("y", 0.5)
-            .set("x", i as f64 * box_width + margin_left)
-            .set("height", 1.5 + EPSILON)
-            .set("width", box_width)
-            .set("fill", "white");
-        let clip_path = element::ClipPath::new().set("id", id.as_str()).add(rect);
+        let x = i as f64 * box_width + IMAGE_WIDTH_Y + COLOR_WIDTH;
 
-        let img = element::Image::new()
-            .set("y", 0.5)
-            .set("x", i as f64 * box_width + margin_left - (0.5 * box_width))
-            .set("width", box_width * 2.0)
-            .set("href", format!("data:image/jpeg;base64,{image}"))
-            .set("clip-path", format!("url(#{})", id.as_str()));
+        let params = ImageParams {
+            x,
+            y: 0.0,
+            height: IMAGE_HEIGHT_X + EPSILON,
+            width: box_width,
+            x_padding: 0.5,
+            y_padding: 0.0,
+            id: &id,
+            image,
+        };
 
-        document.append(img);
-        definitions.append(clip_path);
+        add_image(&mut document, &mut definitions, params);
+
+        let info = &data.x_values[i];
+
+        let params = BoxParams {
+            x,
+            y: IMAGE_HEIGHT_X,
+            width: box_width + EPSILON,
+            height: COLOR_WIDTH + EPSILON,
+        };
+
+        let color_str = match &info.id[..] {
+            [] => COLORLESS_COLOR,
+            [single_color] => &single_color.to_string(),
+            colors => {
+                let id = format!("x-gradient-{}", i);
+                add_gradient_box(&mut document, &mut definitions, params, &id, colors, false);
+                continue;
+            }
+        };
+        add_color_box(&mut document, params, color_str);
     }
-
-    let color_width = 0.2;
 
     for (j, image) in y_images.iter().enumerate() {
         let info = &data.y_values[j];
         let id = format!("y-{j}");
 
-        let inner_y = margin_top + j as f64;
-        let mut outer_y = margin_top + j as f64 - 3.0;
+        let inner_y = IMAGE_HEIGHT_X + COLOR_WIDTH + j as f64;
+        let mut y_offset = 3.0;
 
-        if let Some(y_offset) = info.offset_y {
-            outer_y += y_offset;
+        if let Some(y_offset_offset) = info.offset_y {
+            y_offset -= y_offset_offset;
         }
+        let params = ImageParams {
+            x: 0.0,
+            y: IMAGE_HEIGHT_X + COLOR_WIDTH + j as f64,
+            height: 1.0 + EPSILON,
+            width: IMAGE_WIDTH_Y + EPSILON,
+            x_padding: 0.5,
+            y_padding: y_offset,
+            id: &id,
+            image,
+        };
 
-        assert!(outer_y <= inner_y);
-
-        let rect = Rectangle::new()
-            .set("y", inner_y)
-            .set("x", 0)
-            .set("height", 1.0 + EPSILON)
-            .set("width", margin_left + EPSILON)
-            .set("fill", "white");
-        let clip_path = element::ClipPath::new().set("id", id.as_str()).add(rect);
-
-        let img = element::Image::new()
-            .set("y", outer_y)
-            .set("x", -0.5)
-            .set("width", margin_left + 1.0 + EPSILON)
-            .set("href", format!("data:image/jpeg;base64,{image}"))
-            .set("clip-path", format!("url(#{})", id.as_str()));
-
-        document.append(img);
-        definitions.append(clip_path);
+        add_image(&mut document, &mut definitions, params);
 
         // Add color
+        let params = BoxParams {
+            x: IMAGE_WIDTH_Y,
+            y: inner_y,
+            width: COLOR_WIDTH + EPSILON,
+            height: 1.0 + EPSILON,
+        };
+
         let color_str = match &info.id[..] {
             [] => COLORLESS_COLOR,
             [single_color] => &single_color.to_string(),
-            _ => panic!(),
+            colors => {
+                let id = format!("y-gradient-{}", j);
+                add_gradient_box(&mut document, &mut definitions, params, &id, colors, true);
+                continue;
+            }
         };
-        let rect = Rectangle::new()
-            .set("y", inner_y)
-            .set("x", margin_left - color_width)
-            .set("height", 1.0 + EPSILON)
-            .set("width", color_width + EPSILON)
-            .set("fill", color_str);
-        document.append(rect);
+        add_color_box(&mut document, params, color_str);
     }
 
     document.append(definitions);
@@ -116,8 +222,8 @@ pub fn create_svg(data: &Data, x_images: &[String], y_images: &[String]) -> impl
         outlined_text(
             &mut document,
             "end",
-            margin_left - 0.5,
-            margin_top + j as f64 + 0.5,
+            IMAGE_WIDTH_Y - 0.2,
+            IMAGE_HEIGHT_X + COLOR_WIDTH + j as f64 + 0.5,
             name,
             0.55,
             "serif",
@@ -129,8 +235,8 @@ pub fn create_svg(data: &Data, x_images: &[String], y_images: &[String]) -> impl
         outlined_text(
             &mut document,
             "middle",
-            margin_left + (0.5 + i as f64) * box_width,
-            margin_top - 0.7,
+            IMAGE_WIDTH_Y + COLOR_WIDTH + (0.5 + i as f64) * box_width,
+            IMAGE_HEIGHT_X / 2.0,
             name,
             0.9,
             "serif",
@@ -148,8 +254,8 @@ pub fn create_svg(data: &Data, x_images: &[String], y_images: &[String]) -> impl
                 } else {
                     (MISSING_COLOR, "0".to_string())
                 };
-            let x_pos = i as f64 * box_width + margin_left;
-            let y_pos = j as f64 + margin_top;
+            let x_pos = i as f64 * box_width + IMAGE_WIDTH_Y + COLOR_WIDTH;
+            let y_pos = j as f64 + IMAGE_HEIGHT_X + COLOR_WIDTH;
             let r = Rectangle::new()
                 .set("x", x_pos)
                 .set("y", y_pos)
