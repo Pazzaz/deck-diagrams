d92acdfc61a69ab96f1aaada03b91e1b37e56011
Pazzaz <pazzaz.sundqvist@gmail.com>
2026-08-26 08:58:15 +0200

    Convert to CLI using `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    ffi::OsString,
     fs, io,
     path::{Path, PathBuf},
     str::FromStr,
@@ -21,58 +22,112 @@ mod draw;
 mod partner_data;
 mod web;
 
-fn main() {
-    let data_folder = PathBuf::from_str("./data/partner").unwrap();
-    let download_counts: bool = false;
-    let download_color_ids: bool = false;
-    let download_images: bool = false;
-    let save_data: bool = false;
+use clap::{Args, Parser, Subcommand};
 
-    let f = fs::File::open(data_folder.join("data.json")).unwrap();
+#[derive(Debug, Parser)]
+#[command(name = "deck-diagrams")]
+#[command(about = "CLI to download statistics from EDHREC and generate diagrams", long_about = None)]
+struct Cli {
+    #[arg(help = "Path to JSON file containing statistics")]
+    data: OsString,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Download(DownloadArgs),
+
+    #[command(about = "Render SVG diagram", long_about = None)]
+    #[command(arg_required_else_help = true)]
+    Render {
+        output: OsString,
+    },
+}
+
+#[derive(Debug, Args)]
+#[command(about = "Download statistics", long_about = Some("Download partner/deck data. Provide at least one parameter to start downloading."))]
+#[command(arg_required_else_help = true)]
+// Requires at least one of them to be active
+#[group(required = true)]
+struct DownloadArgs {
+    #[arg(help = "Download number of decks")]
+    #[arg(short = 'd', long)]
+    decks: bool,
+
+    #[arg(help = "Download partners' color identities")]
+    #[arg(long)]
+    ids: bool,
+
+    #[arg(help = "Download partners' images")]
+    #[arg(long)]
+    img: bool,
+}
+
+fn main() {
+    let args = Cli::try_parse().unwrap_or_else(|x| x.exit());
+
+    let data_file = PathBuf::from_str(args.data.to_str().unwrap()).unwrap();
+
+    let data_folder = data_file.parent().unwrap();
+
+    let f = fs::File::open(&data_file).unwrap();
     let mut data: Data = serde_json::from_reader(f).unwrap();
 
-    let to_download = WebParameters::new(download_counts, download_color_ids);
+    match args.command {
+        Commands::Download(DownloadArgs { decks, ids, img }) => {
+            let to_download = WebParameters::new(decks, ids);
 
-    if to_download.any() {
-        let image_path = data_folder.join("images");
-        let images = if download_images { Some(image_path.as_path()) } else { None };
-        update_data(&mut data, to_download, images);
-    }
+            if to_download.any() {
+                let image_path = data_folder.join("images");
+                let images = if img { Some(image_path.as_path()) } else { None };
+                update_data(&mut data, to_download, images);
+            }
 
-    if save_data {
-        let mut out = fs::File::create(data_folder.join("data.json")).unwrap();
+            let mut out = fs::File::create(data_file).unwrap();
 
-        let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
-        let mut ser = serde_json::Serializer::with_formatter(&mut out, formatter);
-        data.serialize(&mut ser).unwrap();
-    }
-
-    let svg = match data {
-        Data::Single(data_single) => {
-            let images = get_images(&data_folder.join("images"), &data_single.values);
-            let order = get_order(&data_single.values, &data_single.decks, Counting::Both);
-            let labels = Labels { values: &data_single.values, order: &order, images: &images };
-
-            create_svg(labels, labels, &data_single.decks)
+            let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
+            let mut ser = serde_json::Serializer::with_formatter(&mut out, formatter);
+            data.serialize(&mut ser).unwrap();
         }
-        Data::Double(data_double) => {
-            let x_images = get_images(&data_folder.join("images"), &data_double.x_values);
-            let y_images = get_images(&data_folder.join("images"), &data_double.y_values);
+        Commands::Render { output } => {
+            let output_path = PathBuf::from_str(output.to_str().unwrap()).unwrap();
+            let svg = match data {
+                Data::Single(data_single) => {
+                    let images = get_images(&data_folder.join("images"), &data_single.values);
+                    let order = get_order(&data_single.values, &data_single.decks, Counting::Both);
+                    let labels =
+                        Labels { values: &data_single.values, order: &order, images: &images };
 
-            let x_order = get_order(&data_double.x_values, &data_double.decks, Counting::X);
-            let y_order = get_order(&data_double.y_values, &data_double.decks, Counting::Y);
+                    create_svg(labels, labels, &data_single.decks)
+                }
+                Data::Double(data_double) => {
+                    let x_images = get_images(&data_folder.join("images"), &data_double.x_values);
+                    let y_images = get_images(&data_folder.join("images"), &data_double.y_values);
 
-            let x_labels =
-                Labels { values: &data_double.x_values, order: &x_order, images: &x_images };
+                    let x_order = get_order(&data_double.x_values, &data_double.decks, Counting::X);
+                    let y_order = get_order(&data_double.y_values, &data_double.decks, Counting::Y);
 
-            let y_labels =
-                Labels { values: &data_double.y_values, order: &y_order, images: &y_images };
+                    let x_labels = Labels {
+                        values: &data_double.x_values,
+                        order: &x_order,
+                        images: &x_images,
+                    };
 
-            create_svg(x_labels, y_labels, &data_double.decks)
+                    let y_labels = Labels {
+                        values: &data_double.y_values,
+                        order: &y_order,
+                        images: &y_images,
+                    };
+
+                    create_svg(x_labels, y_labels, &data_double.decks)
+                }
+            };
+
+            svg::save(output_path, &svg).unwrap();
         }
-    };
-
-    svg::save("./out7.svg", &svg).unwrap();
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
